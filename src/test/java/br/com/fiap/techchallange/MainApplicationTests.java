480571e96142efa48eec1f93bd7a3e36013ced04
axel_kjellin <axel@bussolasocial.com.br>
2024-11-13 15:42:43 -0300

    cucumber config

--- src/test/java/br/com/fiap/techchallange/MainApplicationTests.java
+++ src/test/java/br/com/fiap/techchallange/MainApplicationTests.java
@@ -1,15 +1,11 @@
 package br.com.fiap.techchallange;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-
-@SpringBootTest(classes = MainApplication.class)
 class MainApplicationTests {
 
 	@Test
 	void contextLoads() {
-		// teste actions
 	}
 
 }
